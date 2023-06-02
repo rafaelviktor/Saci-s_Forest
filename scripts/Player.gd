d513a7ffcca13dbd55901383e8629eebb3e3b7cc
Rafael Viktor <rafaellviktor@gmail.com>
2023-06-02 09:17:20 -0300

    melhoria movimentações player

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,16 +1,50 @@
 extends KinematicBody2D
 
 var velocity = Vector2.ZERO
-var movespeed = 200
+var move_speed = 580
 var gravity = 400
-var jumpforce = -180
+var jump_force = -250
+var is_grounded = false
+onready var raycasts = $raycasts
 
 func _physics_process(delta: float) -> void:
-	var move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
 	velocity.y += gravity * delta
-	if Input.is_action_pressed("jump"):
-		velocity.y = jumpforce
 	
-	velocity.x = movespeed * move_direction
-	
+	_get_input()
+
 	move_and_slide(velocity)
+	
+	is_grounded = _check_is_ground()
+	
+	_set_animation()
+
+func _get_input():
+	velocity.x = 0
+	var move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	velocity.x = lerp(velocity.x, move_speed * move_direction, 0.2)
+	
+	if move_direction != 0:
+		$texture.scale.x = move_direction
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("jump") && is_grounded:
+		velocity.y = jump_force
+
+func _check_is_ground():
+	for raycast in raycasts.get_children():
+		if raycast.is_colliding():
+			return true
+	
+	return false
+	
+func _set_animation():
+	var anim = "idle"
+	
+	if !is_grounded:
+		anim = "jump"
+	elif velocity.x != 0:
+		anim = "run"
+		
+	if $animation.assigned_animation != anim:
+		$animation.play(anim)
+		
